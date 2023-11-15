9a68173e6931144c6c72ad019f15acaddb670444
almas-alright <kutsnalmas@gmail.com>
2023-11-15 21:57:27 +0600

    reaction route added

--- app/Http/Controllers/Api/ReactionController.php
+++ app/Http/Controllers/Api/ReactionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Models\Reaction;
 use Illuminate\Http\Request;
 
 class ReactionController extends Controller
@@ -10,8 +12,13 @@ class ReactionController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Post $post)
     {
-        //
+        if($post->isLikedBy(auth()->user())) {
+            Reaction::where('tweet_id', $post->id)->where('user_id', auth()->user()->id)->delete();
+        }else {
+            $post->like(auth()->user());
+        }
+        return back();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -27,11 +27,13 @@ Route::group([
     Route::post('/refresh', [AuthController::class, 'refresh']);
 });
 
-Route::apiResource('post', \App\Http\Controllers\Api\PostController::class)->middleware('jwt');
-Route::post('follow/{user:username}', \App\Http\Controllers\Api\FollowController::class)->middleware('jwt');
+Route::apiResource('posts', \App\Http\Controllers\Api\PostController::class)->middleware('jwt');
 
+Route::post('reactions/{post}/react', \App\Http\Controllers\Api\ReactionController::class);
 
 Route::group(['prefix' => 'user'],function (){
     Route::get('/profile', [AuthController::class, 'userProfile']);
     Route::get('/search', \App\Http\Controllers\Api\UserSearchController::class)->middleware('jwt');
 });
+
+Route::post('/follow/{user:username}', \App\Http\Controllers\Api\FollowController::class)->middleware('jwt');
